1675cc747e856a55add45bd88ec2cce558edf73e
Mandy Wang <shunflower320@gmail.com>
2019-06-17 14:14:41 +0800

    completed add and get functions

--- dbhelper.py
+++ dbhelper.py
@@ -7,12 +7,12 @@ class DBHelper:
         self.conn = sqlite3.connect(dbname)
 
     def setup(self):
-        stmt = "CREATE TABLE IF NOT EXISTS district (postal-code INTEGER, description text, UNIQUE(postal-code))"
+        stmt = "CREATE TABLE IF NOT EXISTS districts (postal_code INTEGER, description text, UNIQUE(postal_code))"
         self.conn.execute(stmt)
         self.conn.commit()
 
     def add_item(self, postal_code, district_text):
-        stmt = "INSERT OR IGNORE INTO district (postal-code, description) VALUES (?)"
+        stmt = "INSERT OR IGNORE INTO districts (postal_code, description) VALUES (?)"
         args = (postal_code, district_text, )
         self.conn.execute(stmt, args)
         self.conn.commit()
@@ -23,6 +23,7 @@ class DBHelper:
 #        self.conn.execute(stmt, args)
 #        self.conn.commit()
 
-    def get_items(self):
-        stmt = "SELECT description FROM items"
-        return [x[0] for x in self.conn.execute(stmt)]
+    def get_items(self, postal_code):
+        stmt = "SELECT description FROM districts WHERE postal_code = (?)"
+        args = (postal_code, )
+        return [x[0] for x in self.conn.execute(stmt, args)]
